90fd946542983031c25e5f29b0f3da3fc58ad014
Gerard Matthew <gerard.matthew@gmail.com>
2023-11-07 13:42:42 +0000

    Add load distribution

--- loadgen/workload_generator.py
+++ loadgen/workload_generator.py
@@ -4,9 +4,14 @@ import numpy as np
 import asyncio
 import random
 import time
+from enum import Enum
+
+class DistributionType(Enum):
+    EXPONENTIAL = 1
+    FIXED = 2
 
 class WorkloadGenerator:
-    def __init__(self, cluster, connections, rps, duration):
+    def __init__(self, cluster, connections, rps, duration, distribution: DistributionType):
         self.connections = connections
         self.cluster = cluster
         self.rps = rps
@@ -14,18 +19,18 @@ class WorkloadGenerator:
         self.results = []
         self.latencies = []
         self.time_elapsed = 0
+        self.distribution = distribution
 
     async def do_connection_work(self, connection_id, request, rps):
-        delays = []
-        # send the request to the cluster
         while (self.time_elapsed < self.duration):
+            # add delay interval before each request to simulate rps
+            delay_between_requests = self.get_delay_between_requests(rps)
+            await asyncio.sleep(delay_between_requests)
+
+            # send the request to the cluster
             response = await self.cluster.process(request)
             self.results.append(response)
             self.latencies.append(response.latency)
-
-            # add delay interval between requests
-            delay_between_requests = self.get_delay_between_requests(rps)
-            await asyncio.sleep(delay_between_requests)
 
             # update the time elapsed
             self.time_elapsed = time.time() - self.start_time
@@ -38,11 +43,14 @@ class WorkloadGenerator:
         # send the request to the cluster
         rps_per_connection = self.rps // self.connections
 
-        print(f'connections: {self.connections} rps: {self.rps} rps_per_connection: {rps_per_connection}')
+        print('Workload Generator Configuration')
+        print('-------------------------------')
+        print(f'Connections: {self.connections} \r\nRequests Per Second: {self.rps} \r\nRequests Per Connection: {rps_per_connection}')
+        print(f'Duration: {self.duration} \r\nDistribution: {self.distribution}')
+        print('-------------------------------')
 
         # start the timer
         self.start_time = time.time()
-       
 
         # run load for the full duration
         tasks = [self.do_connection_work(i, request, rps_per_connection) for i in range(self.connections)]
@@ -54,14 +62,21 @@ class WorkloadGenerator:
         duration = end_time - self.start_time
         requests_per_second = request_count / duration
 
-        print(f'request_count: {request_count} duration: {duration} rps: {requests_per_second}')
+        print('') # newline
+        print('Workload Generator Results')
+        print('--------------------------')
+        print(f'Request Count: {request_count} \r\nActual Requests Per Second: {requests_per_second}')
+        print('--------------------------')
 
         self.generate_report(self.latencies)
 
     def get_delay_between_requests(self, rps):
-        # random.expovariate(rps)
-        return 1 / rps
-
+        if self.distribution == DistributionType.EXPONENTIAL:
+            return random.expovariate(rps)
+        elif self.distribution == DistributionType.FIXED:
+            return 1 / rps
+        else:
+            raise Exception(f'Unknown distribution type: {self.distribution}') 
 
     def generate_report(self, latencies):
         # Calculate latency statistics
@@ -69,11 +84,12 @@ class WorkloadGenerator:
         
         histogram = np.histogram(np_latencies)
         # print percentiles from 0 to 100 in increments of 10
+        print('') # newline
         print('Latency Distribution')
         print('-------------------')
         
         for i in [50, 75, 90, 99, 99.9, 99.99, 99.999, 100]:
             percentile = np.percentile(np_latencies, i)
-            print(f'percentile: {i} latency: {percentile}')
+            print(f'{i}%\t {round(percentile,2)}')
 
         print('-------------------')
--- sim.py
+++ sim.py
@@ -1,11 +1,12 @@
 from simhttp.request import Request
 from cluster import Cluster
 from loadgen.workload_generator import WorkloadGenerator
+from loadgen.workload_generator import DistributionType
 
 import asyncio
 
 if __name__ == "__main__":
     cluster = Cluster()
-    workload_generator = WorkloadGenerator(cluster=cluster, connections=128, rps=1500, duration=10)
+    workload_generator = WorkloadGenerator(cluster=cluster, connections=300, rps=300, duration=10, distribution=DistributionType.EXPONENTIAL)
    
     asyncio.run(workload_generator.do_work())
